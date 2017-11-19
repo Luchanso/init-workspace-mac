0a65a58444e00ca1729f2c7022a700f134d5f24b
Luchansky Oleg <luchansodev@gmail.com>
2017-11-19 22:57:08 +0300

    Init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Init workspace
+
+## Linux
+## MacOS
+```bash
+git clone git@github.com:Luchanso/init-workspace.git
+cd init-workspace/mac
+# after edit script (change emails and names)
+chmod a+x init.sh
+./init.sh
+```
+
+# TODO
+* [ ] Change emails and scripts in script
+* [ ] Add ssh to remote repositories `cat ~/.ssh/id_rsa.pub`
+* [ ]
---
+++ mac/init.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+open https://www.google.com/chrome/browser/desktop/index.html
+open https://code.visualstudio.com/
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew doctor
+brew install git
+
+echo "git config --global user.email "email@example.com""
+echo "git config --global user.name "Luchansky Oleg""
+
+git config --global user.email "email@example.com"
+git config --global user.name "Luchansky Oleg"
+
+brew install node
+brew install yarn
+npm install -g node-gyp n eslint create-react-app http-server
+node --version
+npm --version
+yarn --version
+
+echo "Generate ssh key..."
+ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+
+echo DONE.
