e3d778b58579509146f16a5ab3c9b2b325b051f4
Luchansky Oleg <luchansodev@gmail.com>
2017-11-21 13:18:01 +0300

    Add some scripts

--- mac/init.sh
+++ mac/init.sh
@@ -15,7 +15,7 @@ git config --global user.name "Luchansky Oleg"
 
 brew install node
 brew install yarn
-npm install -g node-gyp n eslint create-react-app http-server
+npm install -g node-gyp n eslint create-react-app http-server eslint-plugin-react, eslint-plugin-jsx-a11y, eslint-plugin-import, eslint-config-airbnb
 node --version
 npm --version
 yarn --version
